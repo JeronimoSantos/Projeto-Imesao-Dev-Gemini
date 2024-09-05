2550b709226a03b521154f5cb45b81e544b45f88
Jeronimo Santos <149694502+JeronimoSantos@users.noreply.github.com>
2024-09-05 00:51:30 -0300

    aula-3

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dadosDev = [
+    {
+        titulo: "HTML",
+        descricao: "Tags HTML são os blocos de construção fundamentais de uma página da web. Elas são como instruções que você dá ao navegador para definir a estrutura e o conteúdo de uma página. Imagine que você está montando um quebra-cabeça, cada peça representa uma tag. Ao encaixar as peças, você cria uma imagem completa.",
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/HTML"
+    },
+    {
+        titulo: "CSS",
+        descricao: "CSS (Cascading Style Sheets) é a linguagem usada para estilizar páginas web. Com CSS, você pode definir a aparência visual de elementos HTML, como cores, fontes, layout e muito mais. É como adicionar um toque de design à sua página.",
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/CSS"
+    },
+    {
+        titulo: "JavaScript",
+        descricao: "JavaScript é uma linguagem de programação que adiciona interatividade às páginas web. Com JavaScript, você pode criar elementos dinâmicos, manipular o DOM, criar animações e muito mais. É como dar vida à sua página.",
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
+    }
+];
