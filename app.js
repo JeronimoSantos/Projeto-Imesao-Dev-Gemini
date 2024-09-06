860a8604cc6af786bdb02aca499fd77cdcb4566a
Jeronimo Santos <149694502+JeronimoSantos@users.noreply.github.com>
2024-09-06 04:39:02 -0300

    Aula-4

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log(dadosDev);
+function pesquisar() {
+    // Seleciona a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+
+    // Itera sobre os dados da pesquisa e cria o HTML para cada resultado
+    for (let dados of dadosDev) {
+        // Cria uma nova div para um resultado
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dados.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dados.descricao}</p>
+                <a href=${dados.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+    }
+
+    // Atualiza o conteúdo da seção com os resultados formatados
+    section.innerHTML = resultados;
+};
--- dados.js
+++ dados.js
@@ -1,17 +1,90 @@
+let dadosCabecalho = [
+];
+
 let dadosDev = [
     {
-        titulo: "HTML",
-        descricao: "Tags HTML são os blocos de construção fundamentais de uma página da web. Elas são como instruções que você dá ao navegador para definir a estrutura e o conteúdo de uma página. Imagine que você está montando um quebra-cabeça, cada peça representa uma tag. Ao encaixar as peças, você cria uma imagem completa.",
-        link: "https://developer.mozilla.org/pt-BR/docs/Web/HTML"
+        titulo: "HTML (HyperText Markup Language)",
+        descricao: "A linguagem padrão para estruturação de páginas web. Define a semântica e a organização do conteúdo.",
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element"
     },
     {
-        titulo: "CSS",
-        descricao: "CSS (Cascading Style Sheets) é a linguagem usada para estilizar páginas web. Com CSS, você pode definir a aparência visual de elementos HTML, como cores, fontes, layout e muito mais. É como adicionar um toque de design à sua página.",
-        link: "https://developer.mozilla.org/pt-BR/docs/Web/CSS"
+        titulo: "CSS (Cascading Style Sheets)",
+        descricao: "Responsável por estilizar a aparência visual das páginas, controlando cores, fontes, layout e responsividade.",
+        link: "https://css-tricks.com/"
     },
     {
         titulo: "JavaScript",
-        descricao: "JavaScript é uma linguagem de programação que adiciona interatividade às páginas web. Com JavaScript, você pode criar elementos dinâmicos, manipular o DOM, criar animações e muito mais. É como dar vida à sua página.",
-        link: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
-    }
+        descricao: "A linguagem de programação que adiciona interatividade às páginas web. Permite criar animações, manipular o DOM (Document Object Model) e construir aplicações complexas.",
+        link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+    },
+    {
+        titulo: "React",
+        descricao: "Biblioteca JavaScript para construção de interfaces de usuário, popular por sua performance e componentes reutilizáveis.",
+        link: "https://react.dev/reference/react"
+    },
+    {
+        titulo: "Angular",
+        descricao: "Framework JavaScript completo para desenvolvimento de aplicações web, oferecendo uma estrutura robusta e rica em recursos",
+        link: "https://v17.angular.io/docs"
+    },
+    {
+        titulo: "Vue.js",
+        descricao: "Framework JavaScript progressivo, conhecido por sua curva de aprendizado suave e flexibilidade.",
+        link: "https://vuejs.org/guide/introduction.html"
+    },
+    {
+        titulo: "Bootstrap",
+        descricao: "Framework CSS que fornece um conjunto de componentes pré-construídos e um sistema de grade responsivo para acelerar o desenvolvimento.",
+        link: "https://getbootstrap.com/docs/5.3/getting-started/introduction/"
+    },
+    {
+        titulo: "Materialize",
+        descricao: "Framework CSS inspirado no Material Design do Google, oferecendo componentes elegantes e personalizados.",
+        link: "https://materializecss.com/"
+    },
+    {
+        titulo: "Sass",
+        descricao: "Framework CSS inspirado no Material Design do Google, oferecendo componentes elegantes e personalizados.",
+        link: "https://materializecss.com/"
+    },
+    {
+        titulo: "Less",
+        descricao: "Framework CSS inspirado no Material Design do Google, oferecendo componentes elegantes e personalizados.",
+        link: "https://materializecss.com/"
+    },
+    {
+        titulo: "JavaScript (Node.js)",
+        descricao: "Ambiente de execução que permite usar JavaScript no lado do servidor, construindo aplicações completas com um único idioma.",
+        link: "https://nodejs.org/docs/latest/api/"
+    },
+    {
+        titulo: "Python (Django, Flask)",
+        descricao: "Linguagem versátil e popular para desenvolvimento web, com frameworks como Django e Flask oferecendo estruturas robustas.",
+        link: "https://docs.python.org/3/"
+    },
+    {
+        titulo: "Ruby (Ruby on Rails)",
+        descricao: "Linguagem conhecida por sua elegância e produtividade, com o framework Ruby on Rails sendo amplamente utilizado para construir aplicações web escaláveis.",
+        link: "https://www.ruby-lang.org/en/documentation/"
+    },
+    {
+        titulo: "PHP (Laravel, Symfony)",
+        descricao: "Linguagem de script originalmente criada para desenvolvimento web, com frameworks como Laravel e Symfony oferecendo recursos modernos e eficientes.",
+        link: "https://www.php.net/docs.php"
+    },
+    {
+        titulo: "SQL (Structured Query Language)",
+        descricao: "Linguagem padrão para gerenciamento de bancos de dados relacionais. É uma ferramenta essencial para qualquer pessoa que trabalhe com bancos de dados relacionais.",
+        link: "https://dev.mysql.com/doc/"
+    },
+    {
+        titulo: "MySQL",
+        descricao: "É um sistema de gerenciamento de banco de dados relacional (SGBDR) amplamente utilizado para armazenar e organizar dados em tabelas relacionadas.",
+        link: "https://docs.oracle.com/cd/E17952_01/index.html"
+    },
+    {
+        titulo: "NoSQL",
+        descricao: "Modelos de dados não relacionais, como MongoDB e Cassandra, utilizados para armazenar grandes volumes de dados com alta escalabilidade.",
+        link: "https://www.mongodb.com/resources/basics/databases/document-databases"
+    },
 ];
