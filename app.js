914b9054e4839fa39f287376e91b6e94a858a0cd
Jeronimo Santos <149694502+JeronimoSantos@users.noreply.github.com>
2024-09-07 09:54:04 -0300

    Aula-5

--- app.js
+++ app.js
@@ -2,22 +2,43 @@ function pesquisar() {
     // Seleciona a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisar").value;
+
+    if (!campoPesquisa) {
+        section.innerHTML = `<p class="cabecalho-paragrafo">Desculpa, Tecnologia não encontrada. Você escreveu nada :(<p>`
+        return
+    };
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+
     // Inicializa uma string vazia para armazenar os resultados formatados
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
 
     // Itera sobre os dados da pesquisa e cria o HTML para cada resultado
     for (let dados of dadosDev) {
-        // Cria uma nova div para um resultado
+        titulo = dados.titulo.toLowerCase()
+        descricao = dados.descricao.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)) {
+
+        // Cria um novo elemento
         resultados += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${dados.titulo}</a>
-                </h2>
-                <p class="descricao-meta">${dados.descricao}</p>
-                <a href=${dados.link} target="_blank">Mais informações</a>
-            </div>
-        `;
-    }
+        <section class="item-resultado">
+            <section class="item-alinhado">
+            <img src="${dados.img}">
+            <h2>
+                <a href="#" target="_blank">${dados.titulo}</a>
+            </h2>
+            </section>
+            <p class="descricao-meta">${dados.descricao}</p>
+            <a href=${dados.link} target="_blank">Documentação para saber mais</a>
+        </section>`
+        } else if (!resultados) {
+            resultados = `<p class="cabecalho-paragrafo">Desculpa, Tecnologia não encontrada :(<p>`
+        }
+    };
 
     // Atualiza o conteúdo da seção com os resultados formatados
     section.innerHTML = resultados;
--- dados.js
+++ dados.js
@@ -3,88 +3,97 @@ let dadosCabecalho = [
 
 let dadosDev = [
     {
-        titulo: "HTML (HyperText Markup Language)",
+        img: "Imagens/Icones/html-5.png",
+        titulo: "Html (HyperText Markup Language)",
         descricao: "A linguagem padrão para estruturação de páginas web. Define a semântica e a organização do conteúdo.",
-        link: "https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element"
+        link: "https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element",
     },
     {
-        titulo: "CSS (Cascading Style Sheets)",
+        img: "Imagens/Icones/css.png",
+        titulo: "Css (Cascading Style Sheets)",
         descricao: "Responsável por estilizar a aparência visual das páginas, controlando cores, fontes, layout e responsividade.",
         link: "https://css-tricks.com/"
     },
     {
+        img: "Imagens/Icones/js.png",
         titulo: "JavaScript",
         descricao: "A linguagem de programação que adiciona interatividade às páginas web. Permite criar animações, manipular o DOM (Document Object Model) e construir aplicações complexas.",
         link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
     },
     {
+        img: "Imagens/Icones/react.png",
         titulo: "React",
         descricao: "Biblioteca JavaScript para construção de interfaces de usuário, popular por sua performance e componentes reutilizáveis.",
         link: "https://react.dev/reference/react"
     },
     {
+        img: "Imagens/Icones/angular.png",
         titulo: "Angular",
         descricao: "Framework JavaScript completo para desenvolvimento de aplicações web, oferecendo uma estrutura robusta e rica em recursos",
         link: "https://v17.angular.io/docs"
     },
     {
+        img: "Imagens/Icones/vue-js.png",
         titulo: "Vue.js",
         descricao: "Framework JavaScript progressivo, conhecido por sua curva de aprendizado suave e flexibilidade.",
         link: "https://vuejs.org/guide/introduction.html"
     },
     {
+        img: "Imagens/Icones/bootstrap.png",   
         titulo: "Bootstrap",
         descricao: "Framework CSS que fornece um conjunto de componentes pré-construídos e um sistema de grade responsivo para acelerar o desenvolvimento.",
         link: "https://getbootstrap.com/docs/5.3/getting-started/introduction/"
     },
     {
+        img: "Imagens/Icones/materialize.png",
         titulo: "Materialize",
         descricao: "Framework CSS inspirado no Material Design do Google, oferecendo componentes elegantes e personalizados.",
         link: "https://materializecss.com/"
     },
-    {
+    {   img: "Imagens/Icones/sass.png",
         titulo: "Sass",
         descricao: "Framework CSS inspirado no Material Design do Google, oferecendo componentes elegantes e personalizados.",
         link: "https://materializecss.com/"
     },
     {
+        img: "Imagens/Icones/less.png",
         titulo: "Less",
         descricao: "Framework CSS inspirado no Material Design do Google, oferecendo componentes elegantes e personalizados.",
         link: "https://materializecss.com/"
     },
     {
+        img: "Imagens/Icones/typescript.png",
+        titulo: "TypeScript",
+        descricao: "TypeScript é uma ferramenta poderosa para desenvolvedores que desejam escrever código JavaScript mais seguro, escalável e manutenível. Ao adicionar tipagem estática e outros recursos avançados.",
+        link: "https://www.typescriptlang.org/docs/"
+    },
+    {
+        img: "Imagens/Icones/node-js.png",
         titulo: "JavaScript (Node.js)",
         descricao: "Ambiente de execução que permite usar JavaScript no lado do servidor, construindo aplicações completas com um único idioma.",
         link: "https://nodejs.org/docs/latest/api/"
     },
-    {
+    {   img: "Imagens/Icones/python.png",
         titulo: "Python (Django, Flask)",
         descricao: "Linguagem versátil e popular para desenvolvimento web, com frameworks como Django e Flask oferecendo estruturas robustas.",
         link: "https://docs.python.org/3/"
     },
     {
+        img: "Imagens/Icones/ruby.png",
         titulo: "Ruby (Ruby on Rails)",
         descricao: "Linguagem conhecida por sua elegância e produtividade, com o framework Ruby on Rails sendo amplamente utilizado para construir aplicações web escaláveis.",
         link: "https://www.ruby-lang.org/en/documentation/"
     },
     {
+        img: "Imagens/Icones/php.png",
         titulo: "PHP (Laravel, Symfony)",
         descricao: "Linguagem de script originalmente criada para desenvolvimento web, com frameworks como Laravel e Symfony oferecendo recursos modernos e eficientes.",
         link: "https://www.php.net/docs.php"
     },
     {
-        titulo: "SQL (Structured Query Language)",
-        descricao: "Linguagem padrão para gerenciamento de bancos de dados relacionais. É uma ferramenta essencial para qualquer pessoa que trabalhe com bancos de dados relacionais.",
-        link: "https://dev.mysql.com/doc/"
-    },
-    {
+        img: "Imagens/Icones/mysql.png",
         titulo: "MySQL",
         descricao: "É um sistema de gerenciamento de banco de dados relacional (SGBDR) amplamente utilizado para armazenar e organizar dados em tabelas relacionadas.",
         link: "https://docs.oracle.com/cd/E17952_01/index.html"
-    },
-    {
-        titulo: "NoSQL",
-        descricao: "Modelos de dados não relacionais, como MongoDB e Cassandra, utilizados para armazenar grandes volumes de dados com alta escalabilidade.",
-        link: "https://www.mongodb.com/resources/basics/databases/document-databases"
     },
 ];
